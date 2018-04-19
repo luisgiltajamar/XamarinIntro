3c281f521b857a91b2da87c9a6d22a7ca57e72ba
programadmalditos@gmail.com <programadmalditos@gmail.com>
2018-04-19 09:23:18 +0200

    Final

---
+++ DemoCanalIntroXamarinForms/DemoCanalIntroXamarinFormsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace DemoCanalIntroXamarinForms
+{
+    public partial class DemoCanalIntroXamarinFormsPage : ContentPage
+    {
+        private int veces = 0;
+
+        public DemoCanalIntroXamarinFormsPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            veces++;
+
+            lbResultado.Text = $"Has pulsado {veces} veces";
+        }
+    }
+}
